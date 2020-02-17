4fa44071ac991388fbf3d442d3c909963d903b10
pankowskitomasz <pankowski_tomasz@o2.pl>
2020-02-17 15:33:20 +0100

    frontend part

---
+++ confirm.php
@@ -0,0 +1,101 @@
+<!DOCTYPE HTML>
+<html lang="de">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,shrink-to-fit=no">
+        <link rel="stylesheet" type="text/css" href="css/styles.min.css">
+        <link rel="stylesheet" type="text/css" href="css/all.min.css">
+        <link rel="icon" href="img/favicon.png"/>
+        <title>Beson | UNIKATE UND EINZIGARTIG AUS WEIMAR</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark navbar-expand-sm position-absolute w-100">
+            <button class="navbar-toggler" 
+                data-toggle="collapse" 
+                data-target="#main-menu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="main-menu">
+                <ul class="nav navbar-nav ml-auto h3">
+                    <li class="nav-item">
+                        <a href="index.html" class="nav-link">
+                            <span class="fa fa-home"></span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://www.facebook.com" 
+                            class="nav-link"
+                            rel="noopener noreferrer nofollow"
+                            target="_blank">
+                            <span class="fab fa-facebook-square"></span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://www.twitter.com" 
+                            class="nav-link"
+                            rel="noopener noreferrer nofollow"
+                            target="_blank">
+                            <span class="fab fa-twitter-square"></span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="user.php" class="nav-link">
+                            <span class="fa fa-user"></span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <section class="container-fluid index-s1 bg-dark text-white d-flex">
+            <div class="row my-auto mx-auto d-flex w-100">
+                <div class="mx-auto text-center text-shadow-black">
+                    <h1 class="display-3 font-weight-bold">BESON</h1>
+                    <h2 class="h4 text-muted font-weight-bold">
+                        <span class="text-white">UNIKATE</span> AUS WEIMAR
+                    </h2>
+                </div>
+            </div>
+        </section>
+        <section class="container error-s2 d-flex py-5" id="message">
+            <div class="row my-auto">
+                <div class="col-10 col-md-8 offset-1 offset-md-2 text-left">                    
+                    <h3 class="text-center mb-3 text-shadow-gray font-weight-bold">
+                        Nachricht gesendet!
+                    </h3>
+                    <div class="text-center">
+                        <span class="far fa-thumbs-up display-4 text-secondary my-4"></span>
+                    </div>
+                    <p class="initialism text-center">
+                        Herzliche Glückwünsche. Ihre Nachricht wurde erfolgreich registriert! 
+                        Bitte denken Sie jedoch daran, dass dies nur eine Demonstration einer 
+                        echten Website ist. Aus diesem Grund wird Ihre Nachricht nicht 
+                        verarbeitet und gespeichert.
+                    </p>
+                </div>
+                <div class="col-12 text-center">
+                    <a href="index.html" class="btn btn-outline-secondary">
+                        <span class="fa fa-home"></span> 
+                        zurück
+                    </a>
+                </div>
+            </div>
+        </section>        
+        <footer class="container-fluid bg-black text-white p-3">
+            <div class="row text-center">
+                <div class="col-12">
+                    <a href="index.html" class="mx-auto">
+                        <img src="img/logo.png" class="img-fluid" alt="beson">
+                    </a>
+		        </div>
+                <div class="col-12">
+                    <small class="text-white-50">
+                        Website demo, Copyright &copy; 2020 |
+                        <a href="privacy.html#terms" class="text-white-50">Privacy</a>
+                    </small>
+                </div>
+            </div>
+        </footer>
+        <script src="js/main.min.js"></script>
+    </body>
+</html>
